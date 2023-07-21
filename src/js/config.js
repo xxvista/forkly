bb43b1fce6117c60d28bd01a5fade7d241ed9199
xxvista <abdelkafiabdellah419@gmail.com>
2023-07-21 07:49:39 +0100

    Added new welcome feature

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,12 @@ export const TIMEOUT_SEC = 13;
 export const RES_PER_PAGE = 10;
 export const KEY = '6d7ee9c1-a0b7-4b0b-815f-d3cbb780533a';
 export const MODAL_CLOSE_SEC = 2.5;
+// git init
+// git add -A
+// git commit -m 'Initial commit'
+// git reset --hard HEAD
+// git log
+// git reset --hard id...
+// git branch
+// git branch new-feature
+// git checkout new-feature
